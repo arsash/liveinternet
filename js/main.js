0546454c5fb6cf11a3d4514b2dc7f16d6e1d0ecd
Arshaluys Aslanyan <a.aslanyan.a@gmail.com>
2020-04-27 00:07:35 +0300

    rating header done

--- js/main.js
+++ js/main.js
@@ -2,8 +2,26 @@ $(document).ready(function (){
 
     $('.hamburger').on('click', () => {
         $('.hamburger__nav').toggleClass('hamburger__nav--active');
-
         $('header').toggleClass('header-active');
-    })
+    });
+
+    $('.country').hover(function () {
+            $('.country__dropdown').css({ 'display' : 'flex' })
+        }, function () {
+            $('.country__dropdown').css({ 'display' : 'none' })
+        }
+    );
+
+    $('.all-regions').hover(function () {
+        $('.all-regions__dropdown').css({ 'display' : 'flex' })
+    }, function () {
+        $('.all-regions__dropdown').css({ 'display' : 'none' })
+    });
+
+    $('.category').hover(function () {
+        $('.category__dropdown').css({ 'display' : 'flex' })
+    }, function () {
+        $('.category__dropdown').css({ 'display' : 'none' })
+    });
 
 });
